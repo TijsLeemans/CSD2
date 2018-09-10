6ecbd9c5a078b1a99fdbf7f24e64433cc1231499
Tijs Leemans <leemanstijs@gmail.com>
2018-09-10 16:02:05 +0200

    Verbeterde versie Helloworld

---
+++ Python Basics/helloworld.py
@@ -0,0 +1,17 @@
+#
+# Opdracht 1 CSD
+#
+
+username = input("Wat is uw naam?")
+
+print("Oh wat een leuke naam", username, "nee hoor grapje...")
+import time
+time.sleep(2)
+print("Ja in weet het, het is een hele flauwe grap. Maar het moetst even.")
+time.sleep(2)
+print("Sorry dat ik je tijd nogsteeds aan het verspillen ben...")
+time.sleep(2)
+print("Naja nee het spijt me eigenlijk helemaal niet..")
+time.sleep(2)
+print("ok doeiiii")
+exit()
