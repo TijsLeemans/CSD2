249a625d22c349f30985862d6e64ea18ccae742f
Tijs Leemans <leemanstijs@gmail.com>
2018-09-13 15:26:42 +0200

    Opdracht 1 t/m 6 uploads.

--- Python Basics/playsound.py
+++ Python Basics/playsound.py
@@ -1,12 +1,23 @@
 import simpleaudio as sa
 import wave
+import time
+n = int(input("zeg het maar, hoe vaak wil je hem hebben? "))
 
-wave_read = wave.open("Audiofiles\PhantomHigh.wav")
-audio_data = wave_read.readframes(wave_read.getnframes())
-num_channels = wave_read.getnchannels()
-bytes_per_sample = wave_read.getsampwidth()
-sample_rate = wave_read.getframerate()
+def player(n):
+    if n == 0:
+        print("okiii klaar")
+        time.sleep(1)
+        return
+    else:
+        print(n)
+        wave_read = wave.open("Audiofiles\snare-1.wav")
+        audio_data = wave_read.readframes(wave_read.getnframes())
+        num_channels = wave_read.getnchannels()
+        bytes_per_sample = wave_read.getsampwidth()
+        sample_rate = wave_read.getframerate()
 
-wave_obj = sa.WaveObject(audio_data, num_channels, bytes_per_sample, sample_rate)
-play_obj = wave_obj.play()
-play_obj.wait_done()
+        wave_obj = sa.WaveObject(audio_data, num_channels, bytes_per_sample, sample_rate)
+        play_obj = wave_obj.play()
+        play_obj.wait_done()
+        return (player(n-1))
+player(n)
